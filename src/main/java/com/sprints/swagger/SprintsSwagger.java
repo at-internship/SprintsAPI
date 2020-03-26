099236db8f7be72a933e9098c01978dda6a32a8d
jonni <jonni.eduardo@agilethought.com>
2020-03-26 12:06:25 -0600

    Change annotation on file SprintsSwagger and rename in file pom.

--- src/main/java/com/sprints/swagger/SprintsSwagger.java
+++ src/main/java/com/sprints/swagger/SprintsSwagger.java
@@ -1,7 +1,7 @@
 package com.sprints.swagger;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -9,7 +9,7 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@Component
+@Configuration
 @EnableSwagger2
 public class SprintsSwagger {
 	@Bean
