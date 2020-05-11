441d5ff17b4648a24af382c480a86c58ac4db968
gabriel.carrasco <gabriel.carrasco@agilethought.com>
2020-05-11 09:51:07 -0500

    Conflict testing class

---
+++ src/main/java/com/sprints/services/TestingConflicts.java
@@ -0,0 +1,13 @@
+package com.sprints.services;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class TestingConflicts {
+	
+	final static Logger logger = LogManager.getLogger(TestingConflicts.class);
+	
+	public void TestingGabriel() {
+		logger.info("Metodo de Gabriel");
+		}
+}
