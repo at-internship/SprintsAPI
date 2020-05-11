b602ee6f1e7bf526c0efc2157c19a233e9bc9a87
jonni <jonni.eduardo@agilethought.com>
2020-05-11 09:50:40 -0500

    Test conflict

---
+++ src/main/java/com/sprints/services/TestingConflicts.java
@@ -0,0 +1,13 @@
+package com.sprints.services;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class TestingConflicts {
+	final static Logger logger = LogManager.getLogger(TestingConflicts.class);
+	
+	public void TestingJonni() {
+		logger.info("Metodo de Jonni");
+	}
+
+}
